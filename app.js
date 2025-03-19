c0b172ec688c4e79f0a9647d1e9e17127cdd300a
Matheus <vtxmatheussamuel@gmail.com>
2025-03-19 14:10:22 -0300

    Tentando Fazer Deploy t-001

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('./config/db');  // Conexão com MongoDB
 const userRoutes = require('./routes/userRoutes');
+const path = require('path');
 
 const app = express();
 
 // Middleware para analisar corpo da requisição
 app.use(bodyParser.json());
 
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index'); 
+});
+
 // Usando rotas
 app.use('/api/users', userRoutes);
 
-// Iniciar o servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+// Iniciar o servidor local
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`);
+// });
+
+// Exportar para o Vercel
+module.exports = app;
